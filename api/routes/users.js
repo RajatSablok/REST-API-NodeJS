c0d60144861e8bb1837290015c07745582c26904
RajatSablok <rajat.main06@gmail.com>
2020-04-18 18:53:11 +0530

    added user login

--- api/routes/users.js
+++ api/routes/users.js
@@ -46,6 +46,34 @@ router.post('/signup', (req, res, next) => {
         })
 });
 
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(402).json({
+                    message: 'Auth failed'
+                })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err) {
+                    return res.status(402).json({
+                        message: 'Auth failed'
+                    })
+                }
+                if (result) {
+                    return res.status(200).json({
+                        message: 'Auth successful'
+                    })
+                }
+                return res.status(402).json({
+                    message: 'Auth failed'
+                })
+            });
+        })
+        .catch()
+});
+
 router.delete('/:userId', (req, res, next) => {
     User.deleteOne({ _id: req.body.userId})
         .exec()
