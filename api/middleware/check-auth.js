fd5af692c897b2b9fca4edc9b8bdedc8a7c14ff3
RajatSablok <rajat.main06@gmail.com>
2020-04-18 21:15:16 +0530

    protected routes

---
+++ api/middleware/check-auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
+        const decoded = jwt.verify(token, process.env.JWT_KEY)
+        req.userData = decoded;
+    } catch {
+        return res.status(401).json({
+            message: 'Auth failed'
+        })
+    }
+
+    next();
+}
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 // const storage = multer.diskStorage({
 //     destination: function(req, fun, cb){
@@ -76,7 +77,7 @@ router.get("/", (req, res, next) => {
 });
 
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
   console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
@@ -136,7 +137,7 @@ router.get("/:productId", (req, res, next) => {
     });
 });
 
-router.patch("/:productId", (req, res, next) => {
+router.patch("/:productId", checkAuth, (req, res, next) => {
   const id = req.params.productId;
   const updateOps = {};
   for (const ops of req.body) {
@@ -161,7 +162,7 @@ router.patch("/:productId", (req, res, next) => {
     });
 });
 
-router.delete("/:productId", (req, res, next) => {
+router.delete("/:productId", checkAuth, (req, res, next) => {
   const id = req.params.productId;
   Product.deleteOne({ _id: id })
     .exec()
