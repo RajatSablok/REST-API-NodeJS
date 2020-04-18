85f70d57a36a13c1e2159154513544e066aa034f
RajatSablok <rajat.main06@gmail.com>
2020-04-18 17:01:23 +0530

    user signup added

---
+++ api/routes/users.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
+
+const User = require('../models/user');
+
+router.post('/signup', (req, res, next) => {
+    User
+        .find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Email aready exists'
+                })
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else{
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user
+                        .save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(201).json({
+                                message: 'User created'
+                            })
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            });
+                        });
+                }
+                });
+            }
+        })
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
 
 const uri = 'mongodb+srv://RajatSablok:3EkKtJoFl0TbtCbM@node-shop-app-gvt3q.mongodb.net/test?retryWrites=true&w=majority'
 
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/users", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
