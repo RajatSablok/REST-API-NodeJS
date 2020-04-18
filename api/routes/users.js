88fc540cbd3299988a235d83f97a9fa3dd3ecefe
RajatSablok <rajat.main06@gmail.com>
2020-04-18 20:35:20 +0530

    Revert "jwt added"
    This reverts commit 154e0d498303943ef306849b3519d02ec2cd0912.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 
 const User = require('../models/user');
 
@@ -63,19 +62,8 @@ router.post('/login', (req, res, next) => {
                     })
                 }
                 if (result) {
-                    const token = jwt.sign({
-                        email: user[0].email,
-                        userId: user[0]._id
-                    },
-                    'secretpasswordlol',
-                    {
-                        expiresIn: '1h'
-                    },
-
-                    )
                     return res.status(200).json({
-                        message: 'Auth successful',
-                        token: token
+                        message: 'Auth successful'
                     })
                 }
                 return res.status(402).json({
