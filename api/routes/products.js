984589e3dbc343375efa1fa397a88f514fe601fc
RajatSablok <rajat.main06@gmail.com>
2020-04-18 15:41:14 +0530

    finished file uploads

--- api/routes/products.js
+++ api/routes/products.js
@@ -19,15 +19,29 @@ const storage = multer.diskStorage({
     filename: function(req, file, cb) {
       cb(null, Date.now()+file.originalname);
     }
-  });
+});
 
-const upload = multer({ storage: storage});
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({ 
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
-    .select("name price _id")
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
       const response = {
@@ -36,6 +50,7 @@ router.get("/", (req, res, next) => {
           return {
             name: doc.name,
             price: doc.price,
+            productImage: doc.productImage,
             _id: doc._id,
             request: {
               type: "GET",
@@ -60,12 +75,14 @@ router.get("/", (req, res, next) => {
     });
 });
 
+
 router.post("/", upload.single('productImage'), (req, res, next) => {
   console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: req.file.path 
   });
   product
     .save()
@@ -95,7 +112,7 @@ router.post("/", upload.single('productImage'), (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(doc => {
       console.log("From database", doc);
--- app.js
+++ app.js
@@ -10,46 +10,47 @@ const orderRoutes = require("./api/routes/orders");
 const uri = 'mongodb+srv://RajatSablok:3EkKtJoFl0TbtCbM@node-shop-app-gvt3q.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose
-     .connect( uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-     .then(() => console.log( 'Database Connected' ))
-     .catch(err => console.log( err ));
+  .connect( uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => console.log( 'Database Connected' ))
+  .catch(err => console.log( err ));
 
-     mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
-     app.use(morgan("dev"));
-     app.use(bodyParser.urlencoded({ extended: false }));
-     app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
      
-     app.use((req, res, next) => {
-       res.header("Access-Control-Allow-Origin", "*");
-       res.header(
-         "Access-Control-Allow-Headers",
-         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-       );
-       if (req.method === "OPTIONS") {
-         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-         return res.status(200).json({});
-       }
-       next();
-     });
-     
-     // Routes which should handle requests
-     app.use("/products", productRoutes);
-     app.use("/orders", orderRoutes);
-     
-     app.use((req, res, next) => {
-       const error = new Error("Not found");
-       error.status = 404;
-       next(error);
-     });
-     
-     app.use((error, req, res, next) => {
-       res.status(error.status || 500);
-       res.json({
-         error: {
-           message: error.message
-         }
-       });
-     });
-     
-     module.exports = app;
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
